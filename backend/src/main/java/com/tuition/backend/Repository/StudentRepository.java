331cb6b5a650022bdd195e665cbfcfc5d93abdc2
ParamiJayasinghe <apurvajayasinghe@gmail.com>
2025-11-08 12:51:09 +0530

    Feat : database configurarions

--- backend/src/main/java/com/tuition/backend/Repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tuition.backend.Repository;
-
-import com.tuition.backend.Entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-    Student findByStudentId(String studentId);
-}
--- backend/src/main/java/com/tuition/backend/Repository/TeacherRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tuition.backend.Repository;
-
-import com.tuition.backend.Entity.Teacher;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeacherRepository extends JpaRepository<Teacher, Long> {
-    Teacher findByTeacherId(String teacherId);
-}
